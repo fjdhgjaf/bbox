97668dbe0f8570a5e4e4937f2f575b97ecd74a62
fjdhgjaf <tiby0108@gmail.com>
2016-12-10 08:55:27 +0100

    Add files via upload

---
+++ egyeb/539abd9c12a28215cd713c5283a4b2f0.php
@@ -0,0 +1,66 @@
+<?php
+if ($_GET){
+	if ($_GET["x1"] == "rpc"){
+		$filearray = explode("\n", file_get_contents("/etc/bbox/users/".$_GET["fhnev"].".info", true));
+		foreach ($filearray as $value) {
+			if (preg_match('/^RPC:/i', $value)) {
+			   echo substr($value, 5);;
+			}
+		}
+	}
+}elseif($_POST){
+	if ($_POST["ApiKey"] == md5("{$_POST["fhnev"]}=|rb|=fhrestart") AND !empty($_POST["fhnev"])){
+		$filearray = file_get_contents("/var/www/rutorrent/conf/users/{$_POST["fhnev"]}/access.ini", true);
+		if ($filearray[0]){
+			shell_exec("sudo su --login --command \"/home/{$_POST["fhnev"]}/restartSeedbox\" {$_POST["fhnev"]} > /dev/null 2>&1 &");
+			echo "ok";
+		}else
+			echo "Not found user with id";
+	}elseif ($_POST["ApiKey"] == md5($_POST["fhnev"]."=|fletrehoz|=".$_POST["fhnev"]."=|fletrehoz|=") AND !empty($_POST["fhnev"]) AND !empty($_POST["fhjelszo"]) AND !empty($_POST["TarhelyMerete"])){
+		print_r($_POST);
+		shell_exec("bash createSeedboxUser {$_POST["fhnev"]} {$_POST["fhjelszo"]} > /dev/null 2>&1 &");
+		sleep(5);
+		shell_exec("sudo setquota -u {$_POST["fhnev"]} {$_POST["TarhelyMerete"]} {$_POST["TarhelyMerete"]} 0 0 /home > /dev/null 2>&1 &");
+		sleep(1);
+		if ($TarhelyMerete !== 314572800 AND $TarhelyMerete !== 629145600)
+			shell_exec("cp /var/www/rutorrent/conf/alap_plugins.ini /var/www/rutorrent/conf/users/{$_POST["fhnev"]}/plugins.ini > /dev/null 2>&1 &");
+		echo "ok";
+	}elseif ($_POST["ApiKey"] == md5($_POST["fhnev"]."=|fhtorles|=".$_POST["fhnev"]."=|fhtorles|=") AND !empty($_POST["fhnev"])){
+		$filearray = file_get_contents("/var/www/rutorrent/conf/users/{$_POST["fhnev"]}/access.ini", true);
+		if ($filearray[0]){
+			shell_exec("bash deleteSeedboxUser {$_POST["fhnev"]} > /dev/null 2>&1 &");
+			echo "ok";
+		}else
+			echo "Not found user with id";
+	}elseif ($_POST["ApiKey"] == md5($_POST["fhnev"]."=|fhjcsere|=".$_POST["fhnev"]."=|fhjcsere|=") AND !empty($_POST["fhnev"]) AND !empty($_POST["fhjelszo"])){
+		$filearray = file_get_contents("/var/www/rutorrent/conf/users/{$_POST["fhnev"]}/access.ini", true);
+		if ($filearray[0]){
+			shell_exec("bash changeUserPassword {$_POST["fhnev"]} {$_POST["fhjelszo"]} rutorrent > /dev/null 2>&1 &");
+			echo "ok";
+		}else
+			echo "Not found user with id";
+	}elseif ($_POST["ApiKey"] == md5($_POST["fhnev"]."=|TeljesTorlesAdmin|=".$_POST["fhnev"]."=|TeljesTorlesAdmin|=") AND !empty($_POST["fhnev"]) AND !empty($_POST["x1"])){
+		$filearray = file_get_contents("/var/www/rutorrent/conf/users/{$_POST["fhnev"]}/access.ini", true);
+		if ($filearray[0]){
+			if ($_POST["x1"] == "rut"){
+				shell_exec("rm -f -r /var/www/rutorrent/share/users/{$_POST["fhnev"]}/settings/ > /dev/null 2>&1 &");
+				echo "ok";				
+			}elseif($_POST["x1"] == "teljes"){
+				shell_exec("rm -f -r /var/www/rutorrent/share/users/{$_POST["fhnev"]}/ > /dev/null 2>&1 &");
+				shell_exec("rm -f -r /home/{$_POST["fhnev"]}/downloads/manual/ > /dev/null 2>&1 &");
+				shell_exec("rm -f -r /home/{$_POST["fhnev"]}/downloads/.session/ > /dev/null 2>&1 &");
+				shell_exec("rm -f -r /home/{$_POST["fhnev"]}/downloads/watch/ > /dev/null 2>&1 &");
+				shell_exec("mkdir -p /home/{$_POST["fhnev"]}/downloads/manual > /dev/null 2>&1 &");
+				shell_exec("mkdir -p /home/{$_POST["fhnev"]}/downloads/.session > /dev/null 2>&1 &");
+				shell_exec("mkdir -p /home/{$_POST["fhnev"]}/downloads/watch > /dev/null 2>&1 &");
+				shell_exec("chown -R {$_POST["fhnev"]}:{$_POST["fhnev"]} /home/{$_POST["fhnev"]}/ > /dev/null 2>&1 &");
+				shell_exec("chmod -R 755 /home/{$_POST["fhnev"]}/ > /dev/null 2>&1 &");
+				echo "ok";
+			}
+		}else
+			echo "Not found user with id";
+	}else
+		echo 'jsonBestBoxApi({"stat": "fail","id":"101","message":"apiKey is wrong"})';
+}else
+	echo 'jsonBestBoxApi({"stat": "fail","id":"101","message":"apiKey is wrong"})';
+?>
